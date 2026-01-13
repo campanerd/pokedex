87563f8d39eae1fd6785ed6e9c6fa0f85aa70448
Davi Campaner <campanerdavi@gmail.com>
2026-01-13 17:24:58 -0300

    ending connections

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn_prev');
+const buttonNext = document.querySelector('.btn_next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     if (APIResponse.status === 200) {
         const data = await APIResponse.json();
@@ -23,6 +27,7 @@ const renderPokemon = async (pokemon) => {
 
     if (data) {
         pokemonName.innerHTML = data.name
+        pokemonImage.style.display = 'block';
         pokemonNumber.innerHTML = ` ${data.id} -`;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
@@ -31,13 +36,31 @@ const renderPokemon = async (pokemon) => {
         } else {
             pokemonName.innerHTML = 'Not found :c';
             pokemonNumber.innerHTML = '';
-            pokemonImage.src = '';
+            pokemonImage.style.display = 'none';
         }
 }
 
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-    renderPokemon(input.value);
-
+    renderPokemon(input.value.toLowerCase());
 });
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+
+buttonNext.addEventListener('click', () => {
+    const currentPokemon = parseInt(pokemonNumber.innerHTML);
+    renderPokemon(currentPokemon + 1);
+});
+
+renderPokemon(searchPokemon);
